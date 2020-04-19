d31434f00054b5a8e882d8ddbe55896f7b049ea3
amincd <Amincd@users.noreply.github.com>
2020-04-19 15:48:10 +0000

    adds specs for AttemptedLinkagesController#index endpoint when the claimed_currency_id is not provided

--- spec/controllers/api/v1/attempted_linkages_controller_spec.rb
+++ spec/controllers/api/v1/attempted_linkages_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 describe Api::V1::AttemptedLinkagesController, type: :controller do
   describe "GET #show" do
     context "when the correct authorization token is provided" do
@@ -80,6 +79,107 @@ describe Api::V1::AttemptedLinkagesController, type: :controller do
         end
 
         it { should respond_with 422 }
+      end
+
+      context "when the user already has an active attempted linkage for the claimed currency" do
+        before(:each) do
+          @attempted_linkage = FactoryBot.create :attempted_linkage
+          @user = @attempted_linkage.user
+
+          # POST #create with user2 to attempt to create an attempted_linkage to the claimed_currency claimed in the last step
+          api_authorization_header @user
+
+          # attempt to create a second attempted_linkage with the same user that created the first attempted_linkage
+          post :create, params: { attempted_linkage: { currency_id_external_key: 10 } }
+        end
+
+        it "returns a json error" do  
+          attempted_linkage_response = json_response
+          expect(attempted_linkage_response[:errors][:user][0]).to include "already has an active attempted linkage"
+        end
+
+        it { should respond_with 422 }
+      end
+    end
+
+    context "when an incorrect authorization token is provided" do
+      before(:each) do
+        api_authorization_header "bogus_token"
+        # attempt to create an attempted_linkage with an incorrect authorization token 
+        post :create, params: { attempted_linkage: { currency_id_external_key: 10 } }
+      end
+
+      it "returns error message" do
+        attempted_linkage_response = json_response
+        expect(attempted_linkage_response[:errors][0][:detail]).to include "You need to sign in"
+      end
+
+      it { should respond_with 401 }
+    end
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      # create first attempted_linkage
+      @attempted_linkage = FactoryBot.create :attempted_linkage
+      @user = @attempted_linkage.user
+
+      # create second attempted_linkage linked to the user that created the first and for a different currency as the validations prevent a user having more than one active attempted_linkage for a particular claimed_currency
+      @attempted_linkage2 = FactoryBot.create :attempted_linkage, currency_id_external_key: 100, user_id: @user.id
+
+      # create third attempted_linkage linked to another user and the same currency linked to the first attempted_linkage
+      @attempted_linkage3 = FactoryBot.create :attempted_linkage, currency_id_external_key: @attempted_linkage.currency_id_external_key 
+      @user2 = @attempted_linkage3
+    end
+
+    context "when the correct authorization token is provided" do
+      context "when a particular claimed_currency_id is provided" do
+        before(:each) do
+          api_authorization_header @user
+          get :index, params: { claimed_currency_id: @attempted_linkage.claimed_currency.id }
+        end
+
+        it "returns 1 record" do
+          attempted_linkages_response = json_response[:data]
+          expect(attempted_linkages_response.length).to eql 1
+        end
+
+        it "returns information about the first attempted_linkage" do
+          attempted_linkages_response = json_response[:data]
+          expect(attempted_linkages_response[0][:attributes][:"claimed-currency-id"]).to eql @attempted_linkage.claimed_currency.id
+        end
+
+        it "returns the user that created the attempted_linkage" do
+          attempted_linkages_response = json_response[:data]
+          expect(attempted_linkages_response[0][:attributes][:"user-id"]).to eql @attempted_linkage.user.id
+        end
+
+        it { should respond_with 200 }
+      end
+
+      context "when no claimed_currency_id is provided" do
+        before(:each) do
+          api_authorization_header @user
+          get :index
+        end
+
+        it "returns 2 records" do
+          attempted_linkages_response = json_response[:data]
+          expect(attempted_linkages_response.length).to eql 2
+        end
+
+        it "returns information about the first attempted_linkage" do
+          attempted_linkages_response = json_response[:data]
+          # the first attempted_linkage is returned as the attempted_linkage collection is reversed in the controller to show the most recent one first
+          expect(attempted_linkages_response[0][:attributes][:"claimed-currency-id"]).to eql @attempted_linkage2.claimed_currency.id
+        end
+
+        it "returns the user that created the attempted_linkage" do
+          attempted_linkages_response = json_response[:data]
+          expect(attempted_linkages_response[0][:attributes][:"user-id"]).to eql @attempted_linkage2.user.id
+        end
+
+        it { should respond_with 200 }
       end
     end
   end
